ce725b3451e15e3015d2998186ed2e97496df85c
TvT669 <3172736837@qq.com>
2025-02-26 15:48:28 +0800

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
